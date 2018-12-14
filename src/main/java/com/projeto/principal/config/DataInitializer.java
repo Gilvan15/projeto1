d55b8e9209ae221de7a8a91d719a6d0b2719b051
Gilvan <gilvan.nascimento@institutoitn.com.br>
2018-12-14 16:40:14 -0400

    métodos mágicos

--- src/main/java/com/projeto/principal/config/DataInitializer.java
+++ src/main/java/com/projeto/principal/config/DataInitializer.java
@@ -5,29 +5,43 @@ import org.springframework.stereotype.Component;
 import com.projeto.principal.entity.Usuario;
 import com.projeto.principal.repository.UserRepository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 @Component
-public class DataInitializer implements ApplicationListener<ContextRefreshedEvent>  {
-	
+public class DataInitializer implements ApplicationListener<ContextRefreshedEvent> {
+
 	@Autowired
 	UserRepository userRepository;
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		
-		Usuario usuario = new Usuario();
+		criarUsuario("Cludionor Sila Medeiros", "claudiosm@gmail.com");
 		
-		usuario.setName("Ramona Morgana Mota Gonçalves");
-		usuario.setEmail("ramona@gmail.com");
-		
-		userRepository.save(usuario);
-		
-		System.out.println("Salvo com sucesso!");
- 		
+		System.out.println("Usuário Salvo!!!");
+
 	}
 	
-
+	public void criarUsuario(String name, String email) {
+		Usuario usuario = new Usuario(name, email);
+		userRepository.save(usuario);
+	}
 }
+
+
+/*
+List<Usuario> usuarios = userRepository.findAll();
+if (usuarios.isEmpty()) {
+	Usuario usuario = new Usuario();
+	usuario.setName("Gilvan Junior Nascimento Gonçalves");
+	usuario.setEmail("gilvan.nascimento@gmail.com");
+	userRepository.save(usuario);
+	System.out.println("Salvo com sucesso!");
+} else {
+	System.out.println("Reginstro já existentes!!");
+}
+*/
